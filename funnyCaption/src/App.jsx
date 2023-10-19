bf503cca1fbd3bf8805a09993a5b750851ac0d54
vijendra2244 <vs230267@gmail.com>
2023-10-19 17:14:31 +0530

    First project in React

---
+++ funnyCaption/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWyuCs3rS9ipW9Am5AhwrJbg-YNT_e5sq4_dwRbQMg&s"
+        alt=""
+      />
+      <h1>Smile,while will still you have a teeth</h1>
+    </>
+  );
+}
+
+export default App;
